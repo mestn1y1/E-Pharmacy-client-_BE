1bc001129d8f2d489b0d2f12a3d7384e9c799843
Serhii Tyron <mestn1y@hotmail.com>
2025-04-01 19:06:56 +0300

    adding start files for running server

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+// Middleware для логування часу запиту
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+// наприклад, у запитах POST або PATCH
+app.use(express.json());
+
+// Маршрут для обробки GET-запитів на '/'
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, World!',
+  });
+});
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
+});
+
+// Middleware для обробких помилок (приймає 4 аргументи)
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
